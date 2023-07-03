574639ba5ba1529fda4934c3c3d6cd5b1c3a1ad0
Alena Kuznetsova <110413634+ALenaKuzDS@users.noreply.github.com>
2023-07-03 17:29:25 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from streamlit import components
+left, middle, right = stl.columns((2, 5, 2))
+with middle:
 with open('data/lda.html', 'r') as f:
     html_string = f.read()
 components.v1.html(html_string, width=1024 , height=768, scrolling=False)
