5f0a751b97d5db15e53d67170d492a5006fdbeb2
Alena Kuznetsova <110413634+ALenaKuzDS@users.noreply.github.com>
2023-07-03 17:27:05 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from streamlit import components
 with open('data/lda.html', 'r') as f:
     html_string = f.read()
-components.v1.html(html_string, width=1600, height=720, scrolling=False)
+components.v1.html(html_string, width=800, height=600, scrolling=False)
 
 
